723666d9a7c1ff55c0932f794e97d4a64abbc37f
Ubuntu <ubuntu@ip-10-32-42-102.eu-west-1.compute.internal>
2012-09-03 12:26:00 +0000

    add recent changes to json_geo and include the line to fetch the data

--- json_geo.py
+++ json_geo.py
@@ -1,40 +1,58 @@
-from mpl_toolkits.basemap import Basemap
-import matplotlib.pyplot as plt
-import numpy as np
-import networkx as nx
 import subprocess as sp
-from text_analysis import *
 from cStringIO import StringIO
 import json
 
-filename = '20120807150601.json.gz'
+#filename = '20120807150601.json.gz'
+filenames =['20120807150301.json.gz',
+            '20120807150601.json.gz',
+            '20120807150901.json.gz',
+            '20120807151201.json.gz',
+            '20120807151501.json.gz',
+            '20120807151734.json.gz',
+            '20120807151958.json.gz',
+            '20120807152202.json.gz',
+            '20120807152302.json.gz',
+            '20120807152401.json.gz',
+            '20120807152502.json.gz',
+            '20120807152601.json.gz',
+            '20120807152701.json.gz',
+            '20120807152801.json.gz',
+            '20120807154101.json.gz',
+            '20120807154201.json.gz',
+            '20120807154301.json.gz',
+            '20120807154402.json.gz',
+            '20120807154501.json.gz',
+            '20120807154601.json.gz',
+            '20120807154702.json.gz',
+            '20120807154801.json.gz',
+            '20120807154901.json.gz',
+            '20120807155001.json.gz',
+            '20120807155101.json.gz',
+            '20120807155201.json.gz',
+            '20120807155302.json.gz',
+            '20120807155402.json.gz',
+            '20120807155501.json.gz',
+            '20120807155601.json.gz',
+            '20120807155701.json.gz',
+            '20120807155801.json.gz',
+            '20120807155902.json.gz',
+            '20120807160002.json.gz',
+            '20120807160101.json.gz']
 
-unzipped_dirty = sp.check_output( [ 'zcat', filename ])
+unzipped_dirty = [sp.check_output( [ 'zcat', f ]) for f in filenames]
+#unzipped_dirty = reduce(str.__add__, unzipped_dirty)
 
-json_data = StringIO(unzipped_dirty.replace(']\n\n[',','))
-tweets = json.load(json_data) 
+json_data = [StringIO(s.replace(']\n\n[',',')) for s in unzipped_dirty]
 
+tweets = []
+for j in json_data:
+    try: tweets = tweets + json.load(j)
+    except: pass
+
+#tweets = [json.load(j) for j in json_data]
+#tweets = reduce(list.__add__, tweets) 
 
 geos = [ t['geo'] for t in tweets ]
 
-m = Basemap(projection='robin',lon_0=0,resolution='c')
-#set a background colour
-m.drawmapboundary(fill_color='#85A6D9')
-m.fillcontinents(color='white',lake_color='#85A6D9')
-m.drawcoastlines(color='#6D5F47', linewidth=.4)
-m.drawcountries(color='#6D5F47', linewidth=.4)
-m.drawmeridians(np.arange(-180, 180, 30), color='#bbbbbb')
-m.drawparallels(np.arange(-90, 90, 30), color='#bbbbbb')
-
-
 lngs_lats = [ g.get('coordinates') for g in geos if g is not None] 
-x,y = m(  [l[1] for l in lngs_lats]  ,  [l[0] for l in lngs_lats])
 
-m.scatter(
-    x,
-    y,
-    c='red', #color
-    marker='o',
-    alpha=0.75, #transparency
-    zorder = 2, #plotting order
-    )
